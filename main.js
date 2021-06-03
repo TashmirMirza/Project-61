e358823ea22b3aef561373f004cefac7c0d470f7
TashmirMirza <84456270+TashmirMirza@users.noreply.github.com>
2021-06-03 18:04:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function mmw() {
+    document.getElementById("mm").src = "https://en.wikipedia.org/wiki/Mason_Mount"
+}
+
+function pfw() {
+    document.getElementById("pf").src = "https://en.wikipedia.org/wiki/Phil_Foden"
+}
+
+function lmw() {
+    document.getElementById("lm").src = "https://en.wikipedia.org/wiki/Lionel_Messi"
+}
